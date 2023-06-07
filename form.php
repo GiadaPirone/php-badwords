54c6c4b4bd6322dc18ec12f999b0b8cc9b470836
GiadaPirone <123646621+GiadaPirone@users.noreply.github.com>
2023-06-07 15:41:28 +0200

    echo paragrafo e input text

---
+++ form.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./style.css">
+    <title>PHP BAD WORDS</title>
+</head>
+<body>
+    <div>
+        <form action="receiveData.php" method="GET">
+            <input type="text" name="titolo" placeholder="Inserisci il titolo della canzone">
+            <textarea name="strofa" placeholder="Inserisci una strofa della tua canzone preferita" id="" cols="30" rows="10"></textarea>
+
+            <button type="submit">Invia</button>
+        </form>
+    </div>
+    
+</body>
+</html>
---
+++ receiveData.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dati ricevuti</title>
+</head>
+<body>
+    <?php
+        $titoloCanzone = $_GET["titolo"];
+        $lenTitolo = strlen($titoloCanzone);
+        
+
+        $strofaCanzone = $_GET["strofa"];
+        $lenStrofa = strlen($strofaCanzone);
+        
+    ?>
+
+    <!-- strofa canzone e lunghezza caratteri -->
+    <div>
+        <h1>Strofa</h1>
+        <p><strong>La strofa della canzone è:</strong>  <?php echo $strofaCanzone ?> </p>
+        <p><strong>La lunghezza della strofa  è:</strong> <?php echo $lenStrofa ?> </p>
+    </div>
+
+    <!-- titolo della canzone nascosto/ da nascondere e lunghezza caratteri -->
+    <div>
+        <h1>Titolo</h1>
+        <p><strong>Il titolo della canzone è:</strong> <?php echo $titoloCanzone ?> </p>
+        <p><strong>La lunghezza del Titolo è:</strong> <?php echo $lenTitolo ?> </p>
+
+    </div>
+
+</body>
+</html>
